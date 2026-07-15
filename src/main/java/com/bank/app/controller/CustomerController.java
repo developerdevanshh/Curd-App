8114927aa02303a2db674c2517b6d8ec3dab3bfa
developerdevanshh <vermadev8749@gmail.com>
2026-07-15 18:30:21 +0530

    GetMapping Implemented

--- src/main/java/com/bank/app/controller/CustomerController.java
+++ src/main/java/com/bank/app/controller/CustomerController.java
@@ -5,10 +5,9 @@ import com.bank.app.repository.CustomerRepository;
 import com.bank.app.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -19,6 +18,11 @@ public class CustomerController {
     @PostMapping()
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
         return ResponseEntity.ok(customerService.createCustomer(customer));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Customer>> getAllCustomer(){
+        return ResponseEntity.ok(customerService.getALlCustomers());
     }
 
 }
